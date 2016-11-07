79ec37430de5494c23e8600922c3254d57f507e1
Yoo-Nah Park <ypark2270@gmail.com>
2016-11-07 12:05:33 -0500

    amy, sean, yoonahs 2nd attempt

--- bash.js
+++ bash.js
@@ -2,24 +2,44 @@ var commandsFile = require('./commands.js');
 
 
 // Output a prompt
-process.stdout.write('Yoo-Nahs prompt > ');
+process.stdout.write('prompt > ');
 
 process.stdin.on('data', function (data) {
   var cmd = data.toString().trim(); // remove the newline
-  if(cmd === "pwd"){
-  	commandsFile.pwd();
+  var word = cmd.split(" "); //[echo, hello, world]
+  var command = word[0]; //[echo, hello, world]
+  var args = word.slice(1).join(" ")
+
+  if(commandsFile.hasOwnProperty(command)){
+    commandsFile[command](args);
   }
-  else if(cmd === "date"){
-  	commandsFile.date();
-  }
-  else if(cmd === 'ls'){
-  	commandsFile.ls();
-  }
-  else {
-  	process.stdout.write('You typed: ' + cmd);
+  else{
+    process.stdout.write('You typed: ' + cmd);
   }
 
-  process.stdout.write('\nprompt > ');
+  // if(command === "pwd"){
+  // 	commandsFile.pwd();
+  // }
+  // else if(command === "date"){
+  // 	commandsFile.date();
+  // }
+  // else if(command === 'ls'){
+  // 	commandsFile.ls();
+  // }
+  // else if(command === 'echo'){
+  //   commandsFile.echo(args);
+  // }
+  // else if (command === 'cat'){
+  //   commandsFile.cat(args);
+  // }
+  // else if (command === 'head'){
+  //   commandsFile.head(args);
+  // }
+  // else {
+  // 	process.stdout.write('You typed: ' + cmd);
+  // }
+
+  
 });
   
 
--- commands.js
+++ commands.js
@@ -1,23 +1,110 @@
 // The stdin 'data' event fires after a user types in a line
 var fs = require('fs');
 
-exports.pwd = function(){
+exports.pwd = function(args){
 	process.stdout.write(process.cwd());
+	process.stdout.write('\nprompt > ');
 }
 
-exports.date = function(){
+exports.date = function(args){
 	process.stdout.write(Date());
+	process.stdout.write('\nprompt > ');
 }
 
-exports.ls = function(){
+exports.ls = function(args){
 	fs.readdir('.', function(err, files) {
 	  if (err) throw err;
 	  files.forEach(function(file) {
 	    process.stdout.write(file.toString() + "\n");
 	  })
-	  //process.stdout.write("prompt > ");
+	  process.stdout.write("\nprompt > ");
 	});
 }
+
+exports.echo = function(args){
+	 if(args[0] == "$"){
+      //environment variable
+      var environ = args.slice(1); //PATH
+      var args = process.env[environ];
+    }
+	process.stdout.write(args);
+	process.stdout.write('\nprompt > ');
+}
+
+exports.cat = function(args){
+	fs.readFile(args, function(err, data){
+		if (err) throw err;
+		process.stdout.write(data);
+		process.stdout.write("\nprompt > ");
+	});
+}
+
+exports.head = function(args){
+	fs.readFile(args, function(err, data){
+		if (err) throw err;
+		var lines = data.toString().split("\n");
+		var result = lines.slice(0,5).join("\n");
+
+		process.stdout.write(result);
+		process.stdout.write("\nprompt > ");
+	});
+}
+
+exports.tail = function(args){
+	fs.readFile(args, function(err, data){
+		if (err) throw err;
+		var lines = data.toString().split("\n");
+		var result = lines.slice(-5).join("\n");
+
+		process.stdout.write(result);
+		process.stdout.write("\nprompt > ");
+	});
+}
+
+exports.sort = function(args){
+	fs.readFile(args, function(err, data){
+		if (err) throw err;
+		var lines = data.toString().split("\n").sort().join("\n");
+		process.stdout.write(lines);
+		process.stdout.write("\nprompt > ");
+	});
+}
+
+exports.wc = function(args){
+	fs.readFile(args, function(err, data){
+		if (err) throw err;
+		var lines = data.toString().split("\n").length.toString();
+		process.stdout.write(lines);
+		process.stdout.write("\nprompt > ");
+	});
+}
+
+exports.uniq = function(args){
+	fs.readFile(args, function(err, data){
+		if (err) throw err;
+		var lines = data.toString().split("\n");
+		var newline = lines.filter(function(current, index, array){
+			if(array.indexOf(current) === index){
+				return true;
+			}
+			else{
+				return false;
+			}
+
+
+			// if(index === 0){
+			// 	return true;
+			// }
+			// if(current === array[index-1]) return false;
+			// else return true;
+		});
+
+		process.stdout.write(newline.join("\n"));
+		process.stdout.write("\nprompt > ");
+	});
+}
+
+
 
 //module.exports;
 //exports{pwd: fn, date: fn}
